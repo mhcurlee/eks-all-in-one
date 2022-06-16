55d92adc181bb637b8faae4daf62e2d93c78770a
Marvin Curlee <marvin.curle@gmail.com>
2022-06-16 09:08:07 -0500

    initial code

---
+++ main.tf
@@ -0,0 +1,408 @@
+
+
+// TF and Providers
+
+terraform {
+  required_version = "~> 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.4"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "~> 1.14"
+    }
+  }
+}
+
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+
+provider "kubectl" {
+  apply_retry_count      = 5
+  load_config_file       = false
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+
+}
+
+
+
+# using locals for testing
+locals {
+  cluster_name    = "karpenter-test"
+  node_group_name = "ng1"
+}
+
+
+
+// VPC
+
+data "aws_availability_zones" "available" {}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.12.0"
+
+  name = local.cluster_name
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+    "karpenter.sh/discovery"                      = local.cluster_name
+  }
+}
+
+
+
+// EKS
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+
+}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  role_principal_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+}
+
+
+# add roles for k8s access
+
+resource "aws_iam_role" "k8s-admin-role" {
+  name = "eks-k8s-admin-role-${local.cluster_name}"
+
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          "AWS" : "${local.role_principal_arn}"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "EKS-${local.cluster_name}"
+  }
+}
+
+
+resource "aws_iam_role" "k8s-dev-role" {
+  name = "eks-k8s-dev-role-${local.cluster_name}"
+
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          "AWS" : "${local.role_principal_arn}"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "EKS-${local.cluster_name}"
+  }
+}
+
+resource "aws_kms_key" "eks" {
+  description             = "EKS Secret Key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+}
+
+
+module "eks" {
+  source                        = "terraform-aws-modules/eks/aws"
+  version                       = "18.20.2"
+  cluster_name                  = local.cluster_name
+  cluster_version               = "1.22"
+  vpc_id                        = module.vpc.vpc_id
+  subnet_ids                    = module.vpc.private_subnets
+  enable_irsa                   = true
+  create_cluster_security_group = false
+  create_node_security_group    = false
+  manage_aws_auth_configmap     = true
+
+  cluster_encryption_config = [
+    {
+      provider_key_arn = aws_kms_key.eks.arn
+      resources        = ["secrets"]
+    }
+  ]
+
+
+  aws_auth_roles = [
+    {
+      rolearn  = aws_iam_role.k8s-admin-role.arn
+      username = "admin-user"
+      groups   = ["system:masters"]
+    },
+    {
+      rolearn  = aws_iam_role.k8s-dev-role.arn
+      username = "dev-user"
+      groups   = [""]
+    }
+  ]
+
+
+
+  eks_managed_node_groups = {
+    (local.node_group_name) = {
+      instance_types                        = ["t3.medium"]
+      ami_type                              = "BOTTLEROCKET_x86_64"
+      create_security_group                 = false
+      attach_cluster_primary_security_group = true
+
+      min_size     = 2
+      max_size     = 2
+      desired_size = 2
+
+      iam_role_additional_policies = [
+        # Required by Karpenter
+        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+      ]
+    }
+  }
+
+  tags = {
+    # Tag node group resources for Karpenter auto-discovery
+    # NOTE - if creating multiple security groups with this module, only tag the
+    # security group that Karpenter should utilize with the following tag
+    "karpenter.sh/discovery" = local.cluster_name
+  }
+}
+
+
+
+
+// Karpenter
+
+resource "aws_iam_instance_profile" "karpenter" {
+  name = "KarpenterNodeInstanceProfile-${local.cluster_name}"
+  role = module.eks.eks_managed_node_groups["${local.node_group_name}"].iam_role_name
+}
+
+
+
+module "karpenter_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "4.17.1"
+
+  role_name                          = "karpenter-controller-${local.cluster_name}"
+  attach_karpenter_controller_policy = true
+
+  karpenter_controller_cluster_id = module.eks.cluster_id
+  karpenter_controller_node_iam_role_arns = [
+    module.eks.eks_managed_node_groups["${local.node_group_name}"].iam_role_arn
+  ]
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["karpenter:karpenter"]
+    }
+  }
+}
+
+
+
+
+resource "helm_release" "karpenter" {
+  namespace        = "karpenter"
+  create_namespace = true
+
+  name       = "karpenter"
+  repository = "https://charts.karpenter.sh"
+  chart      = "karpenter"
+  version    = "v0.10.1"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.karpenter_irsa.iam_role_arn
+  }
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_id
+  }
+
+  set {
+    name  = "clusterEndpoint"
+    value = module.eks.cluster_endpoint
+  }
+
+  set {
+    name  = "aws.defaultInstanceProfile"
+    value = aws_iam_instance_profile.karpenter.name
+  }
+}
+
+
+
+resource "kubectl_manifest" "karpenter_provisioner" {
+  yaml_body = <<-YAML
+  apiVersion: karpenter.sh/v1alpha5
+  kind: Provisioner
+  metadata:
+    name: default
+  spec:
+    limits:
+      resources:
+        cpu: 50
+    provider:
+      amiFamily: Bottlerocket
+      subnetSelector:
+        karpenter.sh/discovery: ${local.cluster_name}
+      securityGroupSelector:
+        karpenter.sh/discovery: ${local.cluster_name}
+      tags:
+        karpenter.sh/discovery: ${local.cluster_name}
+    ttlSecondsAfterEmpty: 30
+    ttlSecondsUntilExpired: 2592000
+  YAML
+
+  depends_on = [
+    helm_release.karpenter
+  ]
+}
+
+
+
+// ALB
+
+
+module "alb_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "4.17.1"
+
+  role_name                              = "alb-controller-${local.cluster_name}"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+
+resource "kubernetes_service_account" "alb_sa" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+      "app.kubernetes.io/component" = "controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn"               = module.alb_irsa.iam_role_arn
+      "eks.amazonaws.com/sts-regional-endpoints" = "true"
+    }
+  }
+}
+
+
+resource "helm_release" "aws-load-balancer-controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  depends_on = [
+    kubernetes_service_account.alb_sa
+  ]
+  set {
+    name  = "clusterName"
+    value = local.cluster_name
+  }
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+  set {
+    name  = "serviceAccount.create"
+    value = "false"
+  }
+
+}
+
+// Metrics Server
+
+resource "helm_release" "metrics-server" {
+  name       = "metrics-server"
+  repository = "https://kubernetes-sigs.github.io/metrics-server"
+  chart      = "metrics-server"
+  namespace  = "kube-system"
+
+}
+
+// Prometheus
+
+resource "helm_release" "prometheus" {
+  name             = "prometheus-community"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "prometheus"
+  namespace        = "prometheus"
+  create_namespace = true
+
+  set {
+    name  = "alertmanager.persistentVolume.storageClass"
+    value = "gp2"
+  }
+  set {
+    name  = "server.persistentVolume.storageClass"
+    value = "gp2"
+  }
+}
